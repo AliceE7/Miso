2880cb83e8b37d8f5edd4175c499093934f401f9
allie <117031743+AliceAsh@users.noreply.github.com>
2023-02-07 15:23:05 +0000

    Initial commit

---
+++ src/bot.js
@@ -0,0 +1,9 @@
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
+const { token } = process.env;
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+  partials: [Partials.Channel, Partials.Guild, Partials.Role]
+});
+
+client.login(token)
