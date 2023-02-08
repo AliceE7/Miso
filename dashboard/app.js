9d4da85448eb6e3dbf79783009461bdd9fc830af
allie <117031743+AliceAsh@users.noreply.github.com>
2023-02-08 21:54:12 +0000

    dashboard changed

--- dashboard/app.js
+++ dashboard/app.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 
 module.exports = client => {
+  app.use(express.static(__dirname + '/public'));
+
+  app.set('views', __dirname + "/views");
+  app.set(`view engine`, "ejs");
+
   app.get(`/`, (req, res) => {
-    res.send(`x`)
+    res.render('index', {
+      client: client
+    })
   })
 
   app.listen(3000, () => { })
--- index.js
+++ index.js
@@ -1,36 +1,56 @@
 console.clear();
 require('./src/bot.js');
-const { sendError } = require("./src/functions/WMAQv-h/handling-functions.js");
+const { sendError } = require("./src/functions/handlers/handling-functions.js");
+/*const { ShardingManager } = require('discord.js');
+const chalk = require('chalk');
+
+const manager = new ShardingManager('./src/bot.js', {
+  token: process.env.token
+});
+
+manager.on("shardCreate", (shard) => {
+  log(`Launching Shard: ${shard}`)
+})*/
+
 
 process.on("rejectionHandled", (promise) => {
   sendError(promise)
-})
+});
 
 process.on("uncaughtException", (error, orgin) => {
   sendError(error, orgin)
-})
+});
 
 process.on("uncaughtExceptionMonitor", (error, orgin) => {
   sendError(error, orgin)
-})
+});
 
 process.on("unhandledRejection", (reason, promise) => {
   sendError(promise, reason)
-})
+});
 
 process.on("warning", (warning) => {
   sendError(warning)
-})
+});
 
 /**  */
 process.on("beforeExit", (code) => {
   sendError(code)
-})
+});
 
 process.on("disconnect", () => {
   sendError("xx", "xx", "xx", "xx")
-})
+});
 
 process.on("exit", (code) => {
   sendError(code)
-})
+});
+
+async function log(x) {
+  console.log(
+    chalk.blue(`[ `),
+    chalk.bgGreen('SHARD'),
+    chalk.blue(' ]'),
+    chalk.red(chalk.bold(x))
+  )
+}
